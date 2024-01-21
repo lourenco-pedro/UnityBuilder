79320d8740a615fa96e3fee234e6bdb5604e6315
Pedro Lourenco <dev.pedropereralouranco@dev.com>
2024-01-21 15:15:34 -0300

    My first commit

---
+++ Editor/Scripts/BuildCmd.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityBuilder
+{
+    [CreateAssetMenu(menuName = "UnityBuilder/BuildCmd", fileName = "BuildCmd")]
+    public class BuildCmd : ScriptableObject
+    {
+        public SO_BuildPipeline pipeline;  
+        
+        public static void Build()
+        {
+            try
+            {
+                Debug.Log("Loading BuildCmd sriptableObject...");
+
+                BuildCmd cmd = Resources.Load<BuildCmd>("BuildCmd");
+
+                Debug.Log($"UnityBuilder will use the {cmd.pipeline.name} pipeline for this build...");
+                Debug.Log("Start building...");
+                
+                SO_BuildPipeline.BuildResult result = cmd.pipeline.Build();
+
+                LogBuildResult(result);
+                
+                if (result == SO_BuildPipeline.BuildResult.SUCCESS)
+                {
+                    EditorApplication.Exit(0);
+                }
+                else
+                {
+                    EditorApplication.Exit(1);
+                }
+
+            }
+            catch(Exception e)
+            {
+                Debug.Log(e.Message);
+                EditorApplication.Exit(1);
+            }
+        }
+
+        static void LogBuildResult(SO_BuildPipeline.BuildResult result)
+        {
+            switch (result)
+            {
+                case SO_BuildPipeline.BuildResult.SUCCESS:
+                    Debug.Log("Build Success!");
+                    break;
+                case SO_BuildPipeline.BuildResult.INVALID_ENVIRONMENTS:
+                    Debug.Log($"Build Failed due to { result }. Please certify the right environment variables " +
+                              $"are created before proceeding with the build.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Editor/Scripts/SO_BuildPipeline.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace UnityBuilder
+{
+    [CreateAssetMenu(menuName = "UnityBuilder/BuilderFile")]
+    public class SO_BuildPipeline : ScriptableObject
+    {
+
+        const string ENV_UNITY_BUILDER_ROOT = "UNITY_BUILDER_ROOT";
+
+        public enum BuildResult
+        {
+            SUCCESS,
+            INVALID_ENVIRONMENTS
+        };
+
+        public BuildTarget BuildTarget => _target;
+        
+        [SerializeField] BuildTarget _target;
+        [SerializeField] BuildOptions _options;
+        [SerializeField] SceneAsset[] _scenesInBuild;
+        
+        public BuildResult Build()
+        {
+            if(!CheckEnvironments())
+                return BuildResult.INVALID_ENVIRONMENTS;
+                
+            string[] levels = _scenesInBuild.Select(AssetDatabase.GetAssetPath).ToArray();
+            
+            string buildPath = Path.Combine(Environment.GetEnvironmentVariable(ENV_UNITY_BUILDER_ROOT), "build_temp", "builder_build.exe");
+
+            BuildReport report = BuildPipeline.BuildPlayer(levels, buildPath, _target, _options);
+
+            return BuildResult.SUCCESS;
+        }
+
+        bool CheckEnvironments()
+        {
+            string[] environments = new[]
+            {
+                ENV_UNITY_BUILDER_ROOT
+            };
+
+            Debug.Log(Environment.GetEnvironmentVariable(ENV_UNITY_BUILDER_ROOT));
+            
+            string[] invalidEnvironments = environments
+                .Where(env=> string.IsNullOrEmpty(Environment.GetEnvironmentVariable(env)))
+                .ToArray();
+
+            if (invalidEnvironments.Length > 0)
+            {
+                Debug.Log($"Could not proceed with the build due to the following invalid Environment variables: { string.Join( ',', invalidEnvironments) }");
+                return false;
+            }
+
+            return true;
+        }
+        
+        [CustomEditor(typeof(SO_BuildPipeline))]
+        public class SO_BuildPipeline_CustomInspector : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                base.OnInspectorGUI();
+
+                bool build = GUILayout.Button("Build!");
+                if (build)
+                {
+                    SO_BuildPipeline pipeLine = target as SO_BuildPipeline;
+                    pipeLine.Build();
+                }
+            }
+        }
+    }
+}
